2a298bbed07913896cafaa789122969db0b11077
kazuki-hirayama <kazuki.hirayama@rakus.co.jp>
2020-03-24 16:41:24 +0900

    退室機能

--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('sendExitMessageEvent', function (data) {
+        const message = `${data.userName}さんが退室しました。`;
+        socket.broadcast.emit('receiveExitMessageEvent', {'message': message});
     });
 };
