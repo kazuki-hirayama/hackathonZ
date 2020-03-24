45614e7d9681fb39fa03b10da66471d47d0421b9
kazuki-hirayama <kazuki.hirayama@rakus.co.jp>
2020-03-24 16:23:32 +0900

    メモにユーザ名を反映する

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,7 +3,7 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = $('#userName').text();
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = userName + "さんのメモ：" + $('#message').val();
     // メモの内容を表示
