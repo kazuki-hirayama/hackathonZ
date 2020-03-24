fec3ec7ef10c93aae33d6e2598fa7ffc2d743a9a
kazuki-hirayama <kazuki.hirayama@rakus.co.jp>
2020-03-24 16:18:45 +0900

    入室機能

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -2,11 +2,12 @@
 
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
-const userName = '';
+const userName = $('#userName').val();
 // 入室メッセージイベントを送信する
+socket.emit('sendEnterMessageEvent', {'userName': userName});
 
 
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('recieveEnterMessageEvent', function (data) {
+    $('#thread').prepend('<p>' + data.message + '</p>');
 });
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('sendEnterMessageEvent', function (data) {
+        const message = `${data.userName}さんが入室しました。`;
+        socket.broadcast.emit('recieveEnterMessageEvent', {'message': message});
     });
 };
